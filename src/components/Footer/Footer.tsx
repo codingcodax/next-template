50eb55a397260b25dcac9765bc8c870ab5e89218
Alexis Guzman <codingcodax@gmail.com>
2023-02-24 20:24:54 -0600

    feat(components): setup footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,8 +1,11 @@
 const Footer = () => {
   return (
-    <div>
-      <p>Footer</p>
-    </div>
+    <footer>
+      <p className='text-center'>
+        Handcrafted with <span className='font-mono'>🤍</span> by{' '}
+        <a href='https://codingcodax.dev'>@codingcodax</a>
+      </p>
+    </footer>
   );
 };
 
