8d9510b5eec4d2dae94510cc279cb16d720ae108
Alexis Guzman <codingcodax@gmail.com>
2023-02-24 20:25:36 -0600

    feat(document): remove min height screen classname

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,7 @@ const Document = () => {
   return (
     <Html lang='en'>
       <Head />
-      <body className='min-h-screen font-sans antialiased'>
+      <body className='font-sans antialiased'>
         <Main />
         <NextScript />
       </body>
