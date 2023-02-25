0e8557d227d802098aa0b32e85a4a235279f7efe
Alexis Guzman <codingcodax@gmail.com>
2023-02-24 18:02:41 -0600

    feat(app): add inter font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import { type AppType } from 'next/app';
+import { Inter as FontSans } from '@next/font/google';
 import { type Session } from 'next-auth';
 import { SessionProvider } from 'next-auth/react';
 import { ThemeProvider } from 'next-themes';
@@ -6,16 +7,29 @@ import { api } from '~/utils/api';
 
 import '~/styles/globals.css';
 
+const fontSans = FontSans({
+  subsets: ['latin'],
+  variable: '--font-sans',
+  display: 'swap',
+});
+
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
   return (
-    <ThemeProvider attribute='class' defaultTheme='system' enableSystem>
-      <SessionProvider session={session}>
-        <Component {...pageProps} />
-      </SessionProvider>
-    </ThemeProvider>
+    <>
+      <style jsx global>{`
+        :root {
+          --font-sans: ${fontSans.style.fontFamily};
+        }
+      `}</style>
+      <ThemeProvider attribute='class' defaultTheme='system' enableSystem>
+        <SessionProvider session={session}>
+          <Component {...pageProps} />
+        </SessionProvider>
+      </ThemeProvider>
+    </>
   );
 };
 
