d4bb155e9bc5d771bffbea1b4fe9fd4ae1250429
Alexis Guzman <codingcodax@gmail.com>
2023-02-25 14:01:32 -0600

    feat(document): add bg and text colors for both themes

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,7 @@ const Document = () => {
   return (
     <Html lang='en'>
       <Head />
-      <body className='font-sans antialiased'>
+      <body className='bg-zinc-50 font-sans text-zinc-700 antialiased dark:bg-zinc-900 dark:text-zinc-300'>
         <Main />
         <NextScript />
       </body>
