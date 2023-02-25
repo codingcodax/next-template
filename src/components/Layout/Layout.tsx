9071c707d3238fa7405ea57f6d49639a56e3d510
Alexis Guzman <codingcodax@gmail.com>
2023-02-25 14:02:27 -0600

    feat(layout): update max width size from 5xl to 3xl

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -7,7 +7,7 @@ type Props = {
 
 const Layout = ({ children }: Props) => {
   return (
-    <div className='mx-auto grid min-h-screen max-w-5xl grid-rows-[auto_1fr_auto] gap-y-20 p-10'>
+    <div className='mx-auto grid min-h-screen max-w-3xl grid-rows-[auto_1fr_auto] gap-y-20 p-10'>
       <Header />
       {children}
       <Footer />
