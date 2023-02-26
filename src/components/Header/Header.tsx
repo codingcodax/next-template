25de1c893c66ee513f5cbd8170e0e1430e0f4338
Alexis Guzman <codingcodax@gmail.com>
2023-02-25 21:29:33 -0600

    feat(header): add active link feauture

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,16 +1,38 @@
 import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { cn } from '~/utils';
 
 const Header = () => {
+  const { asPath: currentUrl } = useRouter();
+
   return (
     <header className='flex h-10 items-center justify-between'>
       <Link href='/'>Logo</Link>
       <nav>
         <ul className='flex space-x-8'>
           <li>
-            <Link href='/'>Home</Link>
+            <Link
+              href='/'
+              className={cn(
+                'font-medium transition-colors',
+                'hover:text-black dark:hover:text-white',
+                currentUrl === '/' ? 'text-black dark:text-white' : ''
+              )}
+            >
+              Home
+            </Link>
           </li>
           <li>
-            <Link href='/about'>About</Link>
+            <Link
+              href='/about'
+              className={cn(
+                'font-medium transition-colors',
+                'hover:text-black dark:hover:text-white',
+                currentUrl === '/about' ? 'text-black dark:text-white' : ''
+              )}
+            >
+              About
+            </Link>
           </li>
         </ul>
       </nav>
