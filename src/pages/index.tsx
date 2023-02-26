8899b040721552d4d93b4f59e9ce63fc9fc6a484
Alexis Guzman <codingcodax@gmail.com>
2023-02-25 21:15:14 -0600

    feat(home): setup page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import { type NextPage } from 'next';
 import Head from 'next/head';
+import { Button, Link } from '~/components/ui';
 
 const Home: NextPage = () => {
   return (
@@ -14,7 +15,36 @@ const Home: NextPage = () => {
         <link rel='icon' href='/favicon.ico' />
       </Head>
       <main>
-        <p>Home page</p>
+        <h1 className='text-xl font-bold text-black dark:text-white'>
+          Next.js Template
+        </h1>
+        <h2>
+          Simple template built with{' '}
+          <Link isExternal href='https://www.tailwind-variants.org/'>
+            Tailwind Variants
+          </Link>
+          ,{' '}
+          <Link isExternal href='https://heroicons.com/'>
+            Hero Icons
+          </Link>{' '}
+          and{' '}
+          <Link isExternal href='https://www.radix-ui.com/'>
+            Radix UI
+          </Link>
+          .
+        </h2>
+        <p>
+          With this, you will have the best way to start a full stack
+          application with type-safe thanks to{' '}
+          <Link isExternal href='https://create.t3.gg/'>
+            T3 Stack
+          </Link>
+          , accessible, with SEO, and the facility to create your component
+          library.
+        </p>
+        <Button href='/about' className='mt-4' intent='secondary'>
+          Learn more
+        </Button>
       </main>
     </>
   );
