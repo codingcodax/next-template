d58509c8b5aec7ceefa2519ed29e4264dbbc1f7f
Alexis Guzman <codingcodax@gmail.com>
2023-02-26 18:06:07 -0600

    feat(header): setup nav link

--- src/components/Header/NavLink.tsx
+++ src/components/Header/NavLink.tsx
@@ -1,8 +1,26 @@
-const NavLink = () => {
+import Link from 'next/link';
+import { cn } from '~/utils';
+
+type Props = {
+  currentUrl: string;
+  name: string;
+  href: string;
+};
+
+const NavLink = ({ currentUrl, name, href }: Props) => {
   return (
-    <div>
-      <p>Nav Link</p>
-    </div>
+    <li>
+      <Link
+        href={href}
+        className={cn(
+          'font-medium transition-colors',
+          'hover:text-black dark:hover:text-white',
+          currentUrl === href ? 'text-black dark:text-white' : ''
+        )}
+      >
+        {name}
+      </Link>
+    </li>
   );
 };
 
