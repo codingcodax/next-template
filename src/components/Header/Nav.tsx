412a4c0fbf5d5585148227254c96aef4daab892d
Alexis Guzman <codingcodax@gmail.com>
2023-02-26 18:06:11 -0600

    feat(header): setup nav

--- src/components/Header/Nav.tsx
+++ src/components/Header/Nav.tsx
@@ -1,8 +1,29 @@
-const Nav = () => {
+import NavLink from './NavLink';
+
+type Props = {
+  currentUrl: string;
+};
+
+const links = [
+  {
+    name: 'Home',
+    href: '/',
+  },
+  {
+    name: 'About',
+    href: '/about',
+  },
+];
+
+const Nav = ({ currentUrl }: Props) => {
   return (
-    <div>
-      <p>Nav</p>
-    </div>
+    <nav>
+      <ul className='flex space-x-8'>
+        {links.map((link) => (
+          <NavLink key={link.name} currentUrl={currentUrl} {...link} />
+        ))}
+      </ul>
+    </nav>
   );
 };
 
