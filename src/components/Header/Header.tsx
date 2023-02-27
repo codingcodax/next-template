235c79fc760b183dff56e5402a3f742ec7455a1a
Alexis Guzman <codingcodax@gmail.com>
2023-02-26 18:06:24 -0600

    refactor(header): use nav component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,7 @@
-import Link from 'next/link';
 import { useRouter } from 'next/router';
-import { cn } from '~/utils';
 
 import Logo from './Logo';
+import Nav from './Nav';
 import ToggleThemeButton from './ToggleThemeButton';
 
 const Header = () => {
@@ -11,34 +10,7 @@ const Header = () => {
   return (
     <header className='flex h-10 items-center justify-between'>
       <Logo />
-      <nav>
-        <ul className='flex space-x-8'>
-          <li>
-            <Link
-              href='/'
-              className={cn(
-                'font-medium transition-colors',
-                'hover:text-black dark:hover:text-white',
-                currentUrl === '/' ? 'text-black dark:text-white' : ''
-              )}
-            >
-              Home
-            </Link>
-          </li>
-          <li>
-            <Link
-              href='/about'
-              className={cn(
-                'font-medium transition-colors',
-                'hover:text-black dark:hover:text-white',
-                currentUrl === '/about' ? 'text-black dark:text-white' : ''
-              )}
-            >
-              About
-            </Link>
-          </li>
-        </ul>
-      </nav>
+      <Nav currentUrl={currentUrl} />
       <ToggleThemeButton />
     </header>
   );
